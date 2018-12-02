1be36abd8e7928fe2564a0462295536f4f9c71f7
goncalochincho <goncalochincho@gmail.com>
2018-12-02 19:04:12 +0000

    revamp number 1

--- G002.lisp
+++ G002.lisp
@@ -1,44 +1,18 @@
 
 (in-package :user)
 
-(defconstant DURACAO_MAX_TURNO 480)
-(defconstant DURACAO_PAUSA 40)
-(defconstant DURACAO_ANTES_REFEICAO 240)
-(defconstant CARACTER_PAUSA 'P)
+(defconstant +duracao-max-turno+ 480)
+(defconstant +duracao-pausa+ 40)
+(defconstant +duracao-antes-refeicao+ 240)
+(defconstant +local-inicial+ 'L1)
 
-(defun car-turno-sem-pausa (turno)
-	
-	"Função car modificada para extrair o primeiro elemento
-	 que não seja o caracter da pausa CARACTER_PAUSA.
-	 Argumentos:
-	 * turno -- Lista de tarefas.
-	 Retorno:
-	 * O primeiro turno da lista"
 
-	(let ((resultado (car turno))
-		  (resto (cdr turno)))
-		 (loop
-		 	(if (eq (car resultado) CARACTER_PAUSA)
-		 		(progn
-		 			(setf resultado (car resto))
-		 			(setf resto (cdr resto)))
-				(return resultado)))))
+(defun last-turno (turno)
 
-(defun last-turno-sem-pausa (turno)
-	
-	"Função last modificada para extrair o ultimo elemento
-	 que não seja o caracter da pausa CARACTER_PAUSA.
-	 Argumentos:
-	 * turno -- Lista de tarefas.
-	 Retorno:
-	 * O ultimo turno da lista"
+	"A função last, quando aplicada a um turno, devolve uma lista com a tarefa. 
+	 Esta função last modificada devolve apenas a tarefa, em vez de uma lista 
+	 com uma tarefa.
 
-	(car-turno-sem-pausa (reverse turno)))
-
-(defun last-tarefa (turno)
-
-	"Função last modificada para devolver apenas a tarefa, 
-	 em vez de uma lista com uma tarefa.
 	 Argumentos:
 	 * turno -- o turno do qual será obtida a última tarefa.
 	 Retorno:
@@ -46,71 +20,118 @@
 
 	 (car (last turno)))
 
-(defun tarefa-pausa-p (tarefa)
 
-	"Verifica se uma tarefa é a tarefa de pausa.
-	 Argumentos:
-	 * tarefa -- Lista de 4 ou 1 elemento(s).
-	 Retorno:
-	 * T se for corresponder a uma tarefa de pausa, NIL caso contrário."
-	 
-	(eq (car tarefa) CARACTER_PAUSA))
-
-(defun duracao-tarefa (tarefa &key (contabiliza-pausas? T))
+(defun duracao-tarefa (tarefa)
 
 	"Calcula a duração de uma tarefa.
+
 	 Argumentos:
-	 * tarefa -- Lista de 4 ou 1 elemento(s).
-	 * contabiliza-pausas? -- booleano que indica se pausas devem ser contabilizadas
+	 * tarefa -- Lista de 4 elementos que representa uma tarefa.
 	 Retorno:
-	 * A duração da tarefa."
-	;(format t "duracao-tarefa ~A ~%" tarefa)
-	(if (tarefa-pausa-p tarefa)
-		(if contabiliza-pausas? 
-			DURACAO_PAUSA
-			0)
-		(- (nth 3 tarefa) (nth 2 tarefa))))
+	 * Um inteiro que representa a duração da tarefa em minutos."
 
-(defun duracao-turno (turno &key (contabiliza-pausas? T))
+	(- (nth 3 tarefa) (nth 2 tarefa)))
 
-	"Calcula a duração de um turno.
+
+(defun duracao-total-turno (turno)
+
+	"Calcula a duração total de um turno, desde o inicio (contabilizando deslocações inicial e final).
+
 	 Argumentos:
 	 * turno -- Lista de tarefas.
-	 * contabiliza-pausas? -- booleano que indica se pausas devem ser contabilizadas
 	 Retorno:
 	 * A duração do turno."
-	;(format t "duracao-turno ~A ~%" turno )
-	(let ((duracao-total (- (nth 3 (last-turno-sem-pausa turno)) (nth 2 (car-turno-sem-pausa turno)))))
-		(if contabiliza-pausas?
-			(let ((num-pausas 0))
-				(if (eq (car (car turno)) CARACTER_PAUSA)
-					(setf num-pausas (1+ num-pausas)))
-				(if (eq (car (last-tarefa turno)) CARACTER_PAUSA)
-					(setf num-pausas (1+ num-pausas)))
-				(setf duracao-total (+ duracao-total (* num-pausas DURACAO_PAUSA)))))
-		duracao-total))
+
+	(let ((num-pausas 0)
+		  (primeira-tarefa (car turno))
+		  (ultima-tarefa (last-turno turno))
+		  (duracao 0))
+
+		;;verifica se existem deslocações inicial e final a contabilizar para a duração
+		(if (not (eq (nth 0 primeira-tarefa) +local-inicial+)) ;inicial
+			(setf num-pausas (1+ num-pausas)))
+		(if (not (eq (nth 1 ultima-tarefa) +local-inicial+)) ;final
+			(setf num-pausas (1+ num-pausas)))
+
+		(setf duracao (- (nth 3 ultima-tarefa) (nth 2 primeira-tarefa)))
+		(setf duracao (+ duracao (* num-pausas +duracao-pausa+)))
+		duracao))
+
+(defun duracao-conducao-turno (turno)
+
+	"Calcula a duração total de condução um turno.
+
+	 Argumentos:
+	 * turno -- Lista de tarefas.
+	 Retorno:
+	 * A duração de condução feita num turno."	
+
+	(let ((duracao 0))
+		(dolist (tarefa turno)
+			(setf duracao (+ duracao (duracao-tarefa tarefa))))
+		duracao))
+
 
 (defun tarefas-sobrepostas-p (tarefa1 tarefa2)
 
 	"Predicado que verifica se duas tarefas se sobrepoem.
+
 	 Argumentos:
 	 * tarefa1 -- Lista de 4 ou 1 elemento(s).
 	 * tarefa2 -- Lista de 4 ou 1 elemento(s).
 	 Retorno:
 	 * T se se sobrepuserem, NIL caso contrário."
-	(and (not (eq (car tarefa2) CARACTER_PAUSA))
-		 (not (eq (car tarefa1) CARACTER_PAUSA))
-		 (< (nth 2 tarefa2) (car (last tarefa1)))))
 
-(defun turno-pausa ()
+	;;se for necessaria uma pausa para deslocação entre as duas tarefas 
+	;;deve ser contabilizado esse tempo extra
+	(let ((pausa? 0)) ;0 ou 1 em vez de booleano para poder ser usado na multiplicação
+		(if (not (eq (nth 1 tarefa1) (nth 0 tarefa2)))
+			(setf pausa? 1))
 
-	"Constrói uma lista com uma lista com o elemento de pausa, 
-	 correspondente a um turno de pausa.
+		(< (nth 2 tarefa2) (+ (nth 3 tarefa1) (* pausa? +duracao-pausa+)))))
+
+(defun cria-problema (input)
+
+	"Le um problema na sua representação externa e transforma-o para a sua
+	 representação interna, transformando cada tarefa num turno.
+
+	 Argumentos:
+	 * input-- lista de tarefas correspondente à 
+	               representação externa de um problema a resolver.
 	 Retorno:
-	 * Uma lista com apenas 1 elemento, o caracter de pausa."
+	 * Lista de turnos correspondente ao estado inicial de um problema."
 
-	 (list (list CARACTER_PAUSA)))
+  (let ((problema '()))
+    (dolist (i input)
+      (setf problema (append problema (list (list i)))))
+  	problema))
 
+
+(defun tem-vaga-p (turno contrario?)
+
+	"Verifica se um turno tem espaço para refeição.
+
+	 Argumentos:
+	 * turno -- lista de tarefas.
+	 * contrario? -- T o início ao fim, NIL caso contrário
+	 Retorno:
+	 * T se existir espaço para refeicão, NIl caso contrário."
+
+	(let ((tamanho (1- (length turno))))
+		(dotimes (i tamanho)
+			(if contrario? 
+				(setf i (1- (- tamanho i))))
+			(let ((t1 (nth i turno))
+				  (t2 (nth (1+ i) turno)))
+				;(format t "t1= ~A  t2= ~A ~%" t1 t2)
+			    ;(format t "aux-vaga= ~A  ~%" (and (eq (nth 0 t2) (nth 1 t1))
+							; (>= (- (nth 2 t2) (nth 3 t1)) +duracao-pausa+)))
+				(if (or (and (eq (nth 0 t2) (nth 1 t1))
+							 (>= (- (nth 2 t2) (nth 3 t1)) +duracao-pausa+));não é necessário um transporte.
+						(and (not (eq (nth 0 t2) (nth 1 t1)))
+							 (>= (- (nth 2 t2) (nth 3 t1)) (* 2 +duracao-pausa+)))) ;é necessario fazer um transporte? 
+					(return-from tem-vaga-p T)))))
+	NIL)
 
 
 (defun une-turnos (turno1 turno2)
@@ -123,80 +144,62 @@
 	 * Novo turno gerado de unir turno1 e turno2 se puderem ser unidos,
 	   NIL caso não seja possível."
 
-	(if (eq (car (last-tarefa turno1)) CARACTER_PAUSA)
-		(setf turno1 (butlast turno1)))
-	(if (eq (car (car turno2)) CARACTER_PAUSA)
-		(setf turno2 (cdr turno2)))
-	(let ((turno-auxiliar turno1)
-		  (turno-resultado '()))
-		(if (and (not (tarefas-sobrepostas-p (last-tarefa turno1) (car turno2)))
-				 (<=  (duracao-turno turno1) DURACAO_MAX_TURNO)
-				 (<=  (duracao-turno turno2) DURACAO_MAX_TURNO))
-			(progn
-				(let ((num_pausas 0)
-					  (chegada-turno1 (nth 1 (last-tarefa turno1)))
-					  (partida-turno2 (car (car turno2))))
-					;(format t "t1= ~A t2= ~A ~%" turno1 turno2)
-					;(format t "ct1= ~A pt2= ~A ~%" chegada-turno1 partida-turno2)
-					;;((L8 L9 89 99)) ((L2 L4 45 55)) destinos e partidas diferentes e ambos diferentes de L1
-					;; necessitam de fazer 2 pausas, voltar a L1 e sair de L1
-					(if (and (not (eq chegada-turno1 'L1))
-						 	 (not (eq partida-turno2 'L1))
-						 	 (not (eq chegada-turno1 partida-turno2)))
-						(setf num_pausas 2))
-					;;(((L8 L1 55 88)) ((L2 L4 45 55)) ou (((L8 L3 55 88)) ((L1 L4 45 55))
-					;; a partida da segunda tarefa é L1, logo apenas é necessaria 1 pausa
-					;; de levar o trabalhador de L3 a L1, equivalente para o primeiro exemplo
-					(if (or (and (eq chegada-turno1 'L1)
-								 (not (eq partida-turno2 'L1)))
-							(and (not (eq chegada-turno1 'L1))
-								 (eq partida-turno2 'L1)))
-						(setf num_pausas 1))
-					;;(((L8 L1 55 88)) ((L1 L4 45 55)) ou (((L8 L9 55 88)) ((L9 L4 45 55))
-					;;neste caso não são necessárias pausas
-					(if (eq chegada-turno1 partida-turno2)
-						(setf num_pausas 0))
-					;;acrescentar as pausas entre os turnos
-					(dotimes (i num_pausas)
-						(setf turno-auxiliar (append turno-auxiliar (turno-pausa)))))
-				(setf turno-auxiliar (append turno-auxiliar turno2))
-				;;refeições!
-				;(format t "pre refeicoes ~A ~%" turno-auxiliar)
-				(let ((duracao-corrente 0))
-					 (dolist (tarefa turno-auxiliar)
-					 	(setf duracao-corrente (+ duracao-corrente (duracao-tarefa tarefa :contabiliza-pausas? NIL)))
-					 	(if (> duracao-corrente DURACAO_ANTES_REFEICAO)
-					 		(setf turno-resultado (append turno-resultado (list (turno-pausa)))))
-					 	(setf turno-resultado (append turno-resultado (list tarefa)))))
-				;(format t "pos refeicoes ~A  ~A minutos ~%" turno-resultado (duracao-turno turno-resultado))
-				;;após cumprir estes requisitos todos o turno continua válido?
-				(if (> (duracao-turno turno-resultado) DURACAO_MAX_TURNO)
-					NIL
-					turno-resultado))
-			NIL)))
+	(let ((primeira-tarefa-t1 (car turno1))
+			(ultima-tarefa-t1 (last-turno turno1))
+			(primeira-tarefa-t2 (car turno2))
+			(ultima-tarefa-t2 (last-turno turno2)))
+		(let
 
-(defun le-problema (problema)
+			((tempo-inicio-t1 (nth 2 primeira-tarefa-t1))
+			(tempo-inicio-t2 (nth 2 primeira-tarefa-t2))
+			(tempo-fim-t1 (nth 3 ultima-tarefa-t1))
+			(tempo-fim-t2 (nth 3 ultima-tarefa-t2))
 
-	"Le um problema na sua representação externa e transforma-o para a sua
-	 representação interna, transformando cada tarefa num turno e introduzindo
-	 as pausas necessárias no inicio e fim do turno.
-	 Argumentos:
-	 * problema -- lista de tarefas correspondente à 
-	               representação externa de um problema a resolver.
-	 Retorno:
-	 * Lista de turnos correspondente ao estado inicial de um problema."
-	
-	(let ((estado-resultado '())
-		  (turno-aux '()))
-		(dolist (tarefa problema)
-			(if (not (eq (car tarefa) 'L1))
-				(setf turno-aux (append turno-aux (turno-pausa))))
-			(setf turno-aux (append turno-aux (list tarefa)))
-			(if (not (eq (nth 1 tarefa) 'L1))
-				(setf turno-aux (append turno-aux (turno-pausa))))
-			(setf estado-resultado (append estado-resultado (list turno-aux)))
-			(setf turno-aux '()))
-		estado-resultado))
+			(local-inicio-t1 (nth 0 primeira-tarefa-t1))
+			(local-inicio-t2 (nth 0 primeira-tarefa-t2))
+			(local-fim-t1 (nth 1 ultima-tarefa-t1))
+			(local-fim-t2 (nth 1 ultima-tarefa-t2))
+
+			(tempo-conducao-t1 (duracao-conducao-turno turno1))
+			(tempo-conducao-t2 (duracao-conducao-turno turno2)))
+
+
+	(if (not (eq local-inicio-t1 +local-inicial+)) 
+		(setf tempo-inicio-t1 (- tempo-inicio-t1 +duracao-pausa+)))
+  
+  	(if (not (eq local-fim-t2 +local-inicial+)) 
+		(setf tempo-fim-t2 (+ tempo-fim-t2 +duracao-pausa+)))
+
+  	(let ((une? NIL))
+  		;(format t "sobrepostas? ~A ~%" (tarefas-sobrepostas-p ultima-tarefa-t1 primeira-tarefa-t2))
+  		;(format t "duracao= ~A ~%" (- tempo-inicio-t2 tempo-fim-t1))
+  		;(format t "aux = ~A ~%" (tem-vaga-p turno1 T))
+		(if (and (<= (- tempo-fim-t2 tempo-inicio-t1) +duracao-max-turno+) ;tempototal < duracao max turno
+				 (not (tarefas-sobrepostas-p ultima-tarefa-t1 primeira-tarefa-t2))
+		         (or (and (> tempo-conducao-t1 +duracao-antes-refeicao+) ;tempo(t1) > 240
+					      (<= tempo-conducao-t2 +duracao-antes-refeicao+)) ; tempo(t1) > 240 AND tempo(t2) <= 240 
+
+					 (and (> tempo-conducao-t2 +duracao-antes-refeicao+) ; tempo(t2) > 240
+		            	  (<= tempo-conducao-t1 +duracao-antes-refeicao+)) ; tempo(t1) <= 240 AND tempo(t2) > 240
+
+ 					 (and (<= tempo-conducao-t1 +duracao-antes-refeicao+)
+					      (<= tempo-conducao-t2 +duracao-antes-refeicao+)
+					      (or (<= (+ tempo-conducao-t1 tempo-conducao-t2) +duracao-antes-refeicao+);tempo(t1) <= 240 AND tempo(t2) <= 240 AND tempo(t1) + tempo(t2) <= 240
+							  (and (> (+ tempo-conducao-t1 tempo-conducao-t2) +duracao-antes-refeicao+) ;tempo(t1) <= 240 AND tempo(t2) <= 240 AND tempo(t1) + tempo(t2) > 240
+							       (or (and (>= (- tempo-inicio-t2 tempo-fim-t1) +duracao-pausa+)
+									  	    (eq local-inicio-t2 local-fim-t1)) ;existe espaço entre os turnos para refeição?
+
+									   (and (>= (- tempo-inicio-t2 tempo-fim-t1) (* 2 +duracao-pausa+))
+									        (not (eq local-inicio-t2 local-fim-t1))) ;existe espaço entre os turnos para refeição e transporte?
+
+									   (tem-vaga-p turno1 T) 
+									   (tem-vaga-p turno2 NIL)))))))
+					(setf une? T))
+		(if une?
+			(append turno1 turno2)
+			NIL)))))
+
+
 
 (defun gera-sucessores (estado)
 
