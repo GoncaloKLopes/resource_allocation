7f7077210983f8ee617bbfe6f82a927fc1c9115d
goncalochincho <goncalochincho@gmail.com>
2018-12-04 11:07:33 +0000

    turno is now a structure, advantage is that tempo-conducao can be stored instead of calculated every time a new shift is created, 20s speedup on p3 with DFS

--- G002.lisp
+++ G002.lisp
@@ -7,6 +7,39 @@
 (defconstant +local-inicial+ 'L1)
 
 
+(defstruct turno
+	tarefas
+	duracao-total
+	duracao-conducao)
+
+
+(defun cria-turno (tarefas
+					&key (duracao-total NIL)
+					(duracao-conducao NIL))
+
+	(if (null duracao-total)
+		(let ((num-pausas 0)
+			  (primeira-tarefa (car tarefas))
+			  (ultima-tarefa (car (last tarefas))))
+
+			;;verifica se existem deslocacoes inicial e final a contabilizar para a duracao
+			(if (not (eq (nth 0 primeira-tarefa) +local-inicial+)) ;inicial
+				(setf num-pausas (1+ num-pausas)))
+			(if (not (eq (nth 1 ultima-tarefa) +local-inicial+)) ;final
+				(setf num-pausas (1+ num-pausas)))
+			(setf duracao-total (+ (- (nth 3 ultima-tarefa) (nth 2 primeira-tarefa)) (* num-pausas +duracao-pausa+)))))
+
+	(if (null duracao-conducao)
+		(progn
+			(setf duracao-conducao 0)
+			(dolist (tarefa tarefas)
+				(setf duracao-conducao (+ duracao-conducao (tarefa-calcula-duracao tarefa))))))
+
+	(make-turno
+		:tarefas tarefas
+		:duracao-total duracao-total
+		:duracao-conducao duracao-conducao))
+
 (defun last-turno (turno)
 
 	"A funcao last, quando aplicada a um turno, devolve uma lista com a tarefa. 
@@ -17,60 +50,25 @@
 	 Retorno:
 	 * Uma lista de 4 elementos, correspondente a uma tarefa."
 
-	 (car (last turno)))
+	 (car (last (turno-tarefas turno))))
 
 
-(defun duracao-tarefa (tarefa )
+(defun tarefa-calcula-duracao (tarefa)
 
 	"Calcula a duracao de uma tarefa.
 	 Argumentos:
 	 * tarefa -- Lista de 4 elementos que representa uma tarefa.
 	 Retorno:
 	 * Um inteiro que representa a duracao da tarefa em minutos."
-	 ;(format t "~A ~%" tarefa)
 	(- (nth 3 tarefa) (nth 2 tarefa)))
-
-
-(defun duracao-total-turno (turno)
-
-	"Calcula a duracao total de um turno, desde o inicio (contabilizando deslocacoes inicial e final).
-	 Argumentos:
-	 * turno -- Lista de tarefas.
-	 Retorno:
-	 * A duracao do turno."
-;(format t "~A ~%" turno)
-	(let ((num-pausas 0)
-		  (primeira-tarefa (car turno))
-		  (ultima-tarefa (last-turno turno)))
-
-		;;verifica se existem deslocacoes inicial e final a contabilizar para a duracao
-		(if (not (eq (nth 0 primeira-tarefa) +local-inicial+)) ;inicial
-			(setf num-pausas (1+ num-pausas)))
-		(if (not (eq (nth 1 ultima-tarefa) +local-inicial+)) ;final
-			(setf num-pausas (1+ num-pausas)))
-		(+ (- (nth 3 ultima-tarefa) (nth 2 primeira-tarefa) (* num-pausas +duracao-pausa+)))))
-
-
-(defun duracao-conducao-turno (turno)
-
-	"Calcula a duracao total de conducao um turno.
-	 Argumentos:
-	 * turno -- Lista de tarefas.
-	 Retorno:
-	 * A duracao de conducao feita num turno."	
-
-	(let ((duracao 0))
-		(dolist (tarefa turno)
-			(setf duracao (+ duracao (duracao-tarefa tarefa))))
-		duracao))
 
 
 (defun tarefas-sobrepostas-p (tarefa1 tarefa2)
 
 	"Predicado que verifica se duas tarefas se sobrepoem.
 	 Argumentos:
-	 * tarefa1 -- Lista de 4 ou 1 elemento(s).
-	 * tarefa2 -- Lista de 4 ou 1 elemento(s).
+	 * tarefa1 -- Lista de 4 elementos.
+	 * tarefa2 -- Lista de 4 elementos.
 	 Retorno:
 	 * T se se sobrepuserem, NIL caso contrario."
 	 ;(format t "~A ~A ~%" tarefa1 tarefa2)
@@ -93,6 +91,7 @@
 	 Retorno:
 	 * T se existir espaco para refeicao, NIl caso contrario."
 
+	(setf turno (turno-tarefas turno))
 	(let ((tamanho (1- (length turno))))
 		(dotimes (i tamanho)
 			(if contrario? 
@@ -119,7 +118,7 @@
 	NIL)
 
 
-(defun une-turnos (turno1 turno2)
+(defun turnos-unificaveis-p (turno1 turno2)
 
 	"Une dois turnos, tendo em conta as restricoes impostas.
 	 Argumentos:
@@ -129,10 +128,10 @@
 	 * Novo turno gerado de unir turno1 e turno2 se puderem ser unidos,
 	   NIL caso nao seja possível."
 
-	;(format t "uniao ~A ~A ~%" turno1 turno2)
-	(let* ((primeira-tarefa-t1 (car turno1))
-		   (ultima-tarefa-t1 (last-turno turno1))
-		   (primeira-tarefa-t2 (car turno2))
+	(let* (
+		   (primeira-tarefa-t1 (car (turno-tarefas turno1)))
+		   (ultima-tarefa-t1 (last-turno  turno1))
+		   (primeira-tarefa-t2 (car (turno-tarefas turno2)))
 		   (ultima-tarefa-t2 (last-turno turno2))
 
 		   (tempo-inicio-t1 (nth 2 primeira-tarefa-t1))
@@ -145,8 +144,10 @@
 		   (local-fim-t1 (nth 1 ultima-tarefa-t1))
 		   (local-fim-t2 (nth 1 ultima-tarefa-t2))
 
-		   (tempo-conducao-t1 (duracao-conducao-turno turno1))
-		   (tempo-conducao-t2 (duracao-conducao-turno turno2)))
+		   (tempo-conducao-t1 (turno-duracao-conducao turno1))
+		   (tempo-conducao-t2 (turno-duracao-conducao turno2))
+
+		   (tempo-conducao-uniao (+ tempo-conducao-t1 tempo-conducao-t2)))
 
 
 		(if (not (eq local-inicio-t1 +local-inicial+)) 
@@ -155,11 +156,12 @@
 	  	(if (not (eq local-fim-t2 +local-inicial+)) 
 			(setf tempo-fim-t2 (+ tempo-fim-t2 +duracao-pausa+)))
 
-	  	(let ((une? NIL))
+	  	(let ((une? NIL)
+	  		  (duracao-uniao (- tempo-fim-t2 tempo-inicio-t1)))
 	  		;(format t "sobrepostas? ~A ~%" (tarefas-sobrepostas-p ultima-tarefa-t1 primeira-tarefa-t2))
 	  		;(format t "duracao= ~A ~%" (- tempo-fim-t2 tempo-inicio-t1))
 	  		;(format t "aux = ~A ~%" (<= tempo-conducao-t1 +duracao-antes-refeicao+))
-			(if (and (<= (- tempo-fim-t2 tempo-inicio-t1) +duracao-max-turno+) ;tempototal < duracao max turno
+			(if (and (<=  duracao-uniao +duracao-max-turno+) ;tempototal < duracao max turno
 					 (not (tarefas-sobrepostas-p ultima-tarefa-t1 primeira-tarefa-t2))
 			         (or (and (> tempo-conducao-t1 +duracao-antes-refeicao+) ;tempo(t1) > 240
 						      (<= tempo-conducao-t2 +duracao-antes-refeicao+)) ; tempo(t1) > 240 AND tempo(t2) <= 240 
@@ -169,8 +171,8 @@
 
 	 					 (and (<= tempo-conducao-t1 +duracao-antes-refeicao+)
 						      (<= tempo-conducao-t2 +duracao-antes-refeicao+)
-						      (or (<= (+ tempo-conducao-t1 tempo-conducao-t2) +duracao-antes-refeicao+);tempo(t1) <= 240 AND tempo(t2) <= 240 AND tempo(t1) + tempo(t2) <= 240
-								  (and (> (+ tempo-conducao-t1 tempo-conducao-t2) +duracao-antes-refeicao+) ;tempo(t1) <= 240 AND tempo(t2) <= 240 AND tempo(t1) + tempo(t2) > 240
+						      (or (<= tempo-conducao-uniao +duracao-antes-refeicao+);tempo(t1) <= 240 AND tempo(t2) <= 240 AND tempo(t1) + tempo(t2) <= 240
+								  (and (> tempo-conducao-uniao +duracao-antes-refeicao+) ;tempo(t1) <= 240 AND tempo(t2) <= 240 AND tempo(t1) + tempo(t2) > 240
 								       (or (and (>= (- tempo-inicio-t2 tempo-fim-t1) +duracao-pausa+)
 										  	    (eq local-inicio-t2 local-fim-t1)) ;existe espaco entre os turnos para refeicao?
 
@@ -180,16 +182,27 @@
 										   (tem-vaga-p turno1 T tempo-fim-t2) 
 										   (tem-vaga-p turno2 NIL tempo-inicio-t1)))))))
 						(setf une? T))
-			(if une?
-				(append turno1 turno2)
-				NIL))))
+			
+			une?)))
 
 
 (defun junta-turnos (estado turno1 index1 turno2 index2)
-  (let ((turnos (copy-list estado)))
-    (cond ((une-turnos turno1 turno2)
-           (setf (nth index1 turnos) (append (nth index1 turnos) turno2))
-           (setf turnos (remove-nth index2 turnos))turnos))))
+
+
+  (let ((novo-estado (copy-list estado)))
+    (cond ((turnos-unificaveis-p turno1 turno2)
+    	(let* ((novas-tarefas (turno-tarefas (nth index1 novo-estado)))
+    		   (nova-duracao-conducao (+ (turno-duracao-conducao turno1)
+    		                             (turno-duracao-conducao turno2))))
+
+			(setf novas-tarefas (append novas-tarefas (turno-tarefas (nth index2 novo-estado))))
+
+			(setf (nth index1 novo-estado) (cria-turno novas-tarefas :duracao-conducao nova-duracao-conducao))
+;;;;
+
+			(setf novo-estado (remove-nth index2 novo-estado)))
+
+			novo-estado))))
 
 
 (defun remove-nth (n list)
@@ -211,21 +224,21 @@
 	 Retorno:
 	 * Lista de estados que representa os estados gerados."
 
-	   (let ((primeiro (first estado))
-        (resto (rest estado))
-        (resto-aux (rest estado))
-        (i 0)
-        (j 1)
-        (resultado '())
-        (juntou? NIL))
-    (loop 
-      (if (null resto) (return))
-      (progn
-        (setf juntou? (junta-turnos estado primeiro i (first resto-aux) j)) 
-        (if juntou?
-            (setf resultado  (append resultado (list juntou?))))
-        (setf resto-aux (rest resto-aux))
-        (setf j (1+ j))
+	(let ((primeiro (first estado))
+		  (resto (rest estado))
+		  (resto-aux (rest estado))
+		  (i 0)
+		  (j 1)
+		  (resultado '())
+		  (juntou? NIL))
+    	(loop 
+      		(if (null resto) (return))
+      		(progn
+        		(setf juntou? (junta-turnos estado primeiro i (first resto-aux) j)) 
+    			(if juntou?
+        			(setf resultado (append resultado (list juntou?))))
+        		(setf resto-aux (rest resto-aux))
+        		(setf j (1+ j))
         (cond ((null resto-aux) 
                (setf primeiro (first resto))
                (setf i (1+ i))
@@ -253,7 +266,6 @@
 	 * estado -- lista de turnos.
 	 Retorno:
 	 * T se for objectivo, NIL caso contrario."
-
 	 (null (operador estado)))
 
 
@@ -269,13 +281,13 @@
 	;(format t " cost ~A ~%" estado)
 	 (let ((duracao 0))
 	 	(dolist (turno estado)
-	 		(setf duracao (+ duracao (duracao-total-turno turno))))
+	 		(setf duracao (+ duracao (turno-duracao-total turno))))
 	 	duracao))
 
 
 (defun conta-pausas (turno)
-  (let ((primeiro (first turno))
-        (resto (rest turno))
+  (let ((primeiro (first (turno-tarefas turno)))
+        (resto (rest (turno-tarefas turno)))
         (resultado 0))
     (loop 
       (if (null resto) (return))
@@ -287,9 +299,6 @@
   (let ((resultado 0))
     (dolist (turno estado) (setf resultado (+ resultado (conta-pausas turno))))
     (setf resultado (+ (list-length estado) (/ resultado 1000)))))
-
-(defun heuristica (estado)
-  (list-length estado))
 
 
 (defun le-estado-inicial (input)
@@ -304,7 +313,9 @@
 
   (let ((problema '()))
     (dolist (i input)
-      (setf problema (append problema (list (list i)))))
+		(let ((novo-turno (cria-turno (list i))))
+
+      		(setf problema (append problema (list novo-turno)))))
   	problema))
 
 
@@ -318,22 +329,34 @@
 	 * Melhor distribuicao de tarefas obtida."
 
 	;;comecar por transformar a lista de tarefas numa lista de turnos
-	(setf problema (cria-problema (le-estado-inicial problema)
-								  (list #'operador)
-								  :objectivo? #'objectivo-p
-								  :heuristica #'heuristica))
-	(let ((solucao NIL))
+	(let ((solucao NIL)
+		  (tempo 0)
+		  (funcao-heuristica NIL))
 		(cond 
 			((equal estrategia "melhor.abordagem")
 				NIl)
 			((equal estrategia "a*.melhor.heuristica")
-				(setf solucao (car (last (car (procura problema "a*"))))))
+				(progn
+					(setf funcao-heuristica #'n-turnos)
+					(setf estrategia "a*")))
 			((equal estrategia "a*.melhor.heuristica.alternativa")
-				NIL)
+				(progn
+					(setf funcao-heuristica #'heuristica-alternativa)
+					(setf estrategia "a*")))
 			((equal estrategia "sondagem.iterativa")
 				NIL)
 			((equal estrategia "ILDS")
 				NIL)
 			((equal estrategia "abordagem.alternativa")
-				(setf solucao (car (last (car (procura problema "profundidade")))))))
-		(cons (cons solucao (custo-estado solucao)) (n-turnos solucao))))
+				(setf estrategia "profundidade")))
+
+		(setf problema (cria-problema (le-estado-inicial problema)
+								  (list #'operador)
+								  :objectivo? #'objectivo-p
+								  :custo #'custo-estado
+								  :heuristica funcao-heuristica))
+		(setf solucao (procura problema estrategia))
+		(setf tempo (nth 1 solucao))
+		(setf solucao (car (last (car solucao))))
+		(cons (cons (cons solucao (custo-estado solucao)) (n-turnos solucao)) (float (/ tempo internal-time-units-per-second)))))
+		
